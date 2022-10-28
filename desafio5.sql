b842015e7a29915f4780770a47d687f9d01597c3
Bruno <brunocfaraco@hotmail.com.br>
2022-10-28 17:24:13 -0300

    feat: :sparkles: desafio5 conclusion

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT 
+    son.songs_name as "cancao", COUNT(his.fk_history_songs_id) AS "reproducoes"
+FROM
+    SpotifyClone.history AS his
+        INNER JOIN
+    SpotifyClone.songs AS son ON son.songs_id=his.fk_history_songs_id
+GROUP BY son.songs_id
+ORDER BY reproducoes DESC, cancao LIMIT 2;
