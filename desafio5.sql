8ca37795c7538963db7dd1df45a1cfe1c66e651b
Bruno <brunocfaraco@hotmail.com.br>
2022-10-28 17:25:51 -0300

    refactor: :recycle: fix desafio5 identation

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,10 @@
 SELECT 
-    son.songs_name as "cancao", COUNT(his.fk_history_songs_id) AS "reproducoes"
+    son.songs_name AS 'cancao',
+    COUNT(his.fk_history_songs_id) AS 'reproducoes'
 FROM
     SpotifyClone.history AS his
         INNER JOIN
-    SpotifyClone.songs AS son ON son.songs_id=his.fk_history_songs_id
+    SpotifyClone.songs AS son ON son.songs_id = his.fk_history_songs_id
 GROUP BY son.songs_id
-ORDER BY reproducoes DESC, cancao LIMIT 2;
+ORDER BY reproducoes DESC , cancao
+LIMIT 2;
